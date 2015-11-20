e9c3ffda53a0525f7fae270a4c6375acc4ed383a
Daryl Bunker <bunker.daryl@gmail.com>
2015-11-20 17:52:47 -0600

    Very first commit.

---
+++ Assets/Scripts/gameManager.cs
@@ -0,0 +1,162 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class gameManager : MonoBehaviour {
+
+    public Text timeRemaining;
+    public Text winner;
+    public Image winnerBG;
+    public bool gameOver = true;
+    public GameObject player1;
+    public GameObject player2;
+    public Image p1_Gold1;
+    public Image p1_Gold2;
+    public Image p2_Gold1;
+    public Image p2_Gold2;
+    public GameObject NextRoundButton;
+    public GameObject NewGameButton;
+    public GameObject RoundCountdownParent;
+    public Text RoundCountdown;
+    public GameObject StartButton;
+
+    private int timeCount = 60;
+    private int player1WinCount = 0;
+    private int player2WinCount = 0;
+	
+	void Start ()
+    {
+
+        timeRemaining.text = timeCount.ToString();
+        StartButton.SetActive(true);
+	
+	}
+
+
+    public void StartGame ()
+    {
+        StartCoroutine(BeginRound());
+    }
+
+
+    private IEnumerator BeginRound ()
+    {
+        StartButton.SetActive(false);
+        RoundCountdownParent.SetActive(true);
+
+        RoundCountdown.text = "3";
+        yield return new WaitForSeconds(1.0f);
+        RoundCountdown.text = "2";
+        yield return new WaitForSeconds(1.0f);
+        RoundCountdown.text = "1";
+        yield return new WaitForSeconds(1.0f);
+        RoundCountdown.text = "GO!";
+        yield return new WaitForSeconds(0.5f);
+        RoundCountdownParent.SetActive(false);
+
+        StartCoroutine(Countdown(timeCount));
+    }
+
+
+    private IEnumerator Countdown(int timeLeft)
+    {
+        gameOver = false;
+
+        yield return new WaitForSeconds(1.0f);
+
+        timeLeft--;
+
+        timeRemaining.text = timeLeft.ToString();
+
+        if (timeLeft > 0)
+            StartCoroutine(Countdown(timeLeft));
+        else
+        {
+            gameOver = true;
+            DeclareWinner();
+        }
+
+    }
+
+
+    private void DeclareWinner()
+    {
+
+        winnerBG.GetComponent<Image>().enabled = true;
+
+        if (player1.GetComponent<playerControl>().playerScoreNum > player2.GetComponent<playerControl>().playerScoreNum)
+        {
+            winner.text = "Player 1 wins!";
+            player1WinCount++;
+
+            if (player1WinCount == 1)
+            {
+                p1_Gold1.GetComponent<Image>().enabled = true;
+                NextRoundButton.SetActive(true);
+            }
+            else if (player1WinCount == 2)
+            {
+                p1_Gold2.GetComponent<Image>().enabled = true;
+                NewGameButton.SetActive(true);
+            }
+        }
+        else if (player2.GetComponent<playerControl>().playerScoreNum > player1.GetComponent<playerControl>().playerScoreNum)
+        {
+            winner.text = "Player 2 wins!";
+
+            player2WinCount++;
+
+            if (player2WinCount == 1)
+            {
+                p2_Gold1.GetComponent<Image>().enabled = true;
+                NextRoundButton.SetActive(true);
+            }
+            else if (player2WinCount == 2)
+            {
+                p2_Gold2.GetComponent<Image>().enabled = true;
+                NewGameButton.SetActive(true);
+            }
+        }
+        else
+        {
+            winner.text = "It's a tie!";
+            NextRoundButton.SetActive(true);
+        }
+
+    }
+
+
+    public void NewRound()
+    {
+        winnerBG.GetComponent<Image>().enabled = false;
+        winner.text = "";
+        NextRoundButton.SetActive(false);
+
+        player1.GetComponent<playerControl>().Reset();
+        player2.GetComponent<playerControl>().Reset();
+
+        Start();
+    }
+
+
+    public void NewGame ()
+    {
+        NewRound();
+
+        player1WinCount = 0;
+        player2WinCount = 0;
+
+        p1_Gold1.GetComponent<Image>().enabled = false;
+        p1_Gold2.GetComponent<Image>().enabled = false;
+        p2_Gold1.GetComponent<Image>().enabled = false;
+        p2_Gold2.GetComponent<Image>().enabled = false;
+
+        NewGameButton.SetActive(false);
+    }
+
+	
+	void Update ()
+    {
+	
+	}
+}
---
+++ Assets/Scripts/playerControl.cs
@@ -0,0 +1,379 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class playerControl : MonoBehaviour {
+
+    public int playerScoreNum = 0;
+    public bool hasFallen = false;
+	public bool canMove = false;
+    public bool isMoving = false;
+	public Vector3 deathTargetPos = Vector3.zero;
+	public GameObject deathUp;
+	public GameObject deathDown;
+	public GameObject deathRight;
+	public GameObject deathLeft;
+	public Text playerScore;
+	public Image joystickImage;
+	public GameObject otherPlayer1;
+
+    private int currentFloat = 0;
+	private float xPos;
+	private float yPos;
+	private bool resetJoystick = true;
+	private bool moveOffScreen = false;
+	private string moveDirection = "";
+	private GameObject[] iceFloats = new GameObject[35];
+	private Vector3 targetPos = new Vector3(-100, -100, -100);
+	private Color originColor;
+
+	void Start () 
+	{
+
+        originColor = joystickImage.color;
+
+		for (int i=0; i<35; i++)
+		{
+			iceFloats[i] = GameObject.Find("iceFloat_" + i.ToString());
+		}
+
+		if (gameObject.name == "player1")
+		{
+			gameObject.transform.position = new Vector3(iceFloats[34].transform.position.x, iceFloats[34].transform.position.y, 0.0f);
+			currentFloat = 34;
+		}
+		else if (gameObject.name == "player2")
+		{
+			gameObject.transform.position = new Vector3(iceFloats[28].transform.position.x, iceFloats[28].transform.position.y, 0.0f);
+			currentFloat = 28;
+		}
+		
+		canMove = true;
+	
+	}
+
+
+	void MovePlayer(string direction)
+	{
+
+		if (direction == "right")
+		{
+			if (currentFloat != 6 && currentFloat != 13 && currentFloat != 20 && currentFloat != 27 && currentFloat != 34)
+				currentFloat++;
+			else
+			{
+				moveOffScreen = true;
+				currentFloat -= 6;
+			}
+		}
+		else if (direction == "left")
+		{
+			if (currentFloat != 0 && currentFloat != 7 && currentFloat != 14 && currentFloat != 21 && currentFloat != 28)
+				currentFloat--;
+			else
+			{
+				moveOffScreen = true;
+				currentFloat += 6;
+			}
+		}
+		else if (direction == "up")
+		{
+			if (currentFloat >= 7)
+				currentFloat -= 7;
+			else
+			{
+				moveOffScreen = true;
+				currentFloat += 28;
+			}
+		}
+		else if (direction == "down")
+		{
+			if (currentFloat < 28)
+				currentFloat += 7;
+			else
+			{
+				moveOffScreen = true;
+				currentFloat -= 28;
+			}
+		}
+
+		targetPos = new Vector3(iceFloats[currentFloat].transform.position.x, iceFloats[currentFloat].transform.position.y, 0.0f);
+
+		canMove = false;
+        isMoving = true;
+		resetJoystick = false;
+
+	}
+
+
+	void OnTriggerEnter2D (Collider2D hit)
+	{
+        if (hit.gameObject.tag == "Player")
+        {
+            //need to account for both players moving at the same time, running into each other
+            if (isMoving == true && hit.gameObject.GetComponent<playerControl>().isMoving == false && hit.gameObject.GetComponent<playerControl>().hasFallen == false)
+            {
+                if (moveDirection == "right")
+                {
+                    hit.gameObject.GetComponent<playerControl>().deathTargetPos = hit.gameObject.GetComponent<playerControl>().deathRight.transform.position;
+                }
+                else if (moveDirection == "left")
+                {
+                    hit.gameObject.GetComponent<playerControl>().deathTargetPos = hit.gameObject.GetComponent<playerControl>().deathLeft.transform.position;
+                }
+                else if (moveDirection == "up")
+                {
+                    hit.gameObject.GetComponent<playerControl>().deathTargetPos = hit.gameObject.GetComponent<playerControl>().deathUp.transform.position;
+                }
+                else if (moveDirection == "down")
+                {
+                    hit.gameObject.GetComponent<playerControl>().deathTargetPos = hit.gameObject.GetComponent<playerControl>().deathDown.transform.position;
+                }
+
+                hit.gameObject.GetComponent<playerControl>().canMove = false;
+                hit.gameObject.GetComponent<playerControl>().hasFallen = true;
+                hit.gameObject.GetComponent<playerControl>().joystickImage.color = new Color(0.25f, 0.25f, 0.25f, 1);
+
+                playerScoreNum++;
+
+                if (gameObject.name == "player1")
+                {
+                    playerScore.text = "P1:  " + playerScoreNum.ToString();
+                }
+                else if (gameObject.name == "player2")
+                {
+                    playerScore.text = "P2:  " + playerScoreNum.ToString();
+                }
+            }
+        }
+
+	}
+
+
+	private IEnumerator ResetPenguin ()
+	{
+
+        bool tempContinue = false;
+
+		gameObject.GetComponent<SpriteRenderer>().enabled = false;
+
+		if (gameObject.name == "player1")
+		{
+			if (otherPlayer1.GetComponent<playerControl>().currentFloat != 34)
+			{
+				gameObject.transform.position = new Vector3(iceFloats[34].transform.position.x, iceFloats[34].transform.position.y, 0.0f);
+				currentFloat = 34;
+			}
+			else
+			{
+				yield return new WaitForSeconds(1.0f);
+
+                StartCoroutine(ResetPenguin());
+
+                tempContinue = true;
+			}
+		}
+		else if (gameObject.name == "player2")
+		{
+            if (otherPlayer1.GetComponent<playerControl>().currentFloat != 28)
+            {
+                gameObject.transform.position = new Vector3(iceFloats[28].transform.position.x, iceFloats[28].transform.position.y, 0.0f);
+                currentFloat = 28;
+            }
+            else
+            {
+                yield return new WaitForSeconds(1.0f);
+
+                StartCoroutine(ResetPenguin());
+
+                tempContinue = true;
+            }
+		}
+
+        if (tempContinue == false)
+        {
+            yield return new WaitForSeconds(1.0f);
+
+            gameObject.GetComponent<SpriteRenderer>().enabled = true;
+
+            joystickImage.color = originColor;
+
+            canMove = true;
+        }
+		
+	}
+
+
+    public void Reset ()
+    {
+        StopAllCoroutines();
+
+        playerScoreNum = 0;
+        if (gameObject.name == "player1")
+        {
+            playerScore.text = "P1:  " + playerScoreNum.ToString();
+        }
+        else if (gameObject.name == "player2")
+        {
+            playerScore.text = "P2:  " + playerScoreNum.ToString();
+        }
+
+        hasFallen = false;
+        canMove = false;
+        isMoving = false;
+
+        gameObject.GetComponent<SpriteRenderer>().enabled = true;
+
+        deathTargetPos = Vector3.zero;
+
+        resetJoystick = true;
+        moveOffScreen = false;
+        moveDirection = "";
+        joystickImage.color = originColor;
+
+        targetPos = new Vector3(-100, -100, -100);
+
+        Start();
+    }
+
+
+    void Update()
+    {
+
+        if (Camera.main.GetComponent<gameManager>().gameOver == false)
+        {
+            if (gameObject.name == "player1")
+            {
+                xPos = CrossPlatformInputManager.GetAxis("Horizontal_1");
+                yPos = CrossPlatformInputManager.GetAxis("Vertical_1");
+            }
+            else if (gameObject.name == "player2")
+            {
+                xPos = CrossPlatformInputManager.GetAxis("Horizontal_2");
+                yPos = CrossPlatformInputManager.GetAxis("Vertical_2");
+            }
+
+            if (Mathf.Abs(xPos) <= 0.15f && Mathf.Abs(yPos) <= 0.15f)
+            {
+                resetJoystick = true;
+            }
+
+            if (canMove == true && resetJoystick == true)
+            {
+                if (Mathf.Abs(xPos) > Mathf.Abs(yPos))
+                {
+                    if (xPos >= 0.85f)
+                    {
+                        moveDirection = "right";
+                        MovePlayer(moveDirection);
+                    }
+                    else if (xPos <= -0.85f)
+                    {
+                        moveDirection = "left";
+                        MovePlayer(moveDirection);
+                    }
+                }
+                else if (Mathf.Abs(yPos) > Mathf.Abs(xPos))
+                {
+                    if (yPos >= 0.85f)
+                    {
+                        moveDirection = "up";
+                        MovePlayer(moveDirection);
+                    }
+                    else if (yPos <= -0.85f)
+                    {
+                        moveDirection = "down";
+                        MovePlayer(moveDirection);
+                    }
+                }
+            }
+            else
+            {
+                if (hasFallen == true)
+                {
+                    if (gameObject.transform.position != deathTargetPos)
+                    {
+                        gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, deathTargetPos, 0.125f);
+
+                        if (Vector3.Distance(gameObject.transform.position, deathTargetPos) < 0.05f)
+                        {
+                            gameObject.transform.position = deathTargetPos;
+                        }
+                    }
+                    else
+                    {
+                        StartCoroutine(ResetPenguin());
+                        hasFallen = false;
+                    }
+                }
+                else if (gameObject.GetComponent<Renderer>().enabled == true)
+                {
+                    if (moveOffScreen == false)
+                    {
+                        if (targetPos != new Vector3(-100, -100, -100))
+                        {
+                            if (gameObject.transform.position != targetPos)
+                            {
+                                if (Vector3.Distance(gameObject.transform.position, targetPos) < 0.05f)
+                                {
+                                    isMoving = false;
+                                }
+
+                                gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, targetPos, 0.125f);
+                            }
+                            else
+                            {
+                                canMove = true;
+                            }
+                        }
+
+                    }
+                    else
+                    {
+                        if (moveDirection == "right")
+                        {
+                            if (gameObject.GetComponent<Renderer>().isVisible == true)
+                                gameObject.transform.Translate(Vector3.right * 0.175f);
+                            else
+                            {
+                                gameObject.transform.position = new Vector3(-12.0f, transform.position.y, 0.0f);
+                                moveOffScreen = false;
+                            }
+                        }
+                        else if (moveDirection == "left")
+                        {
+                            if (gameObject.GetComponent<Renderer>().isVisible == true)
+                                gameObject.transform.Translate(Vector3.left * 0.175f);
+                            else
+                            {
+                                gameObject.transform.position = new Vector3(12.0f, transform.position.y, 0.0f);
+                                moveOffScreen = false;
+                            }
+                        }
+                        else if (moveDirection == "up")
+                        {
+                            if (gameObject.GetComponent<Renderer>().isVisible == true)
+                                gameObject.transform.Translate(Vector3.up * 0.175f);
+                            else
+                            {
+                                gameObject.transform.position = new Vector3(transform.position.x, -8.0f, 0.0f);
+                                moveOffScreen = false;
+                            }
+                        }
+                        else if (moveDirection == "down")
+                        {
+                            if (gameObject.GetComponent<Renderer>().isVisible == true)
+                                gameObject.transform.Translate(Vector3.down * 0.175f);
+                            else
+                            {
+                                gameObject.transform.position = new Vector3(transform.position.x, 8.0f, 0.0f);
+                                moveOffScreen = false;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
